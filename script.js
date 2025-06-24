c0ee7c0307709cd050b47931bdf1065f3aad078e
abood_lasheen <137902284+hglsmj@users.noreply.github.com>
2025-06-24 21:15:29 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let input = document.getElementById('input');
+let button = document.getElementById('btn');
+let joke = document.getElementById('joke');
+let result = document.getElementById('result');
+let date;
+let alldays;
+let day;
+let jokes = ['Let me see the calendar...just kidding',
+   'Can I see the calendar ?', 'I am busy for this !!!' , 'give me money first...just joking','why do you want to see that ??','check the result after a few years' , 'I am working do not bother me please','can you check it yourself ?','I am tired right now','Is it really important ???'
+]
+button.onclick = function getday(){
+   date = new Date(input.value);
+   alldays = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+   day = date.getDay();
+   if(input.value = '' || alldays[day] == undefined){
+      alert('please enter a valid date')
+   }else{setTimeout(function(){
+    result.innerHTML = alldays[day]  
+   },5000)
+   joke.innerHTML = jokes[Math.floor(Math.random() * 10)]
+   input.onfocus = function(){
+   location.reload()
+   }}
+}
