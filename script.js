53620720dccb5074c604449176a081eaefc7b121
abood_lasheen <137902284+hglsmj@users.noreply.github.com>
2025-06-28 09:05:11 +0300

    Update script.js

--- script.js
+++ script.js
@@ -8,16 +8,14 @@ let day;
 let jokes = ['Let me see the calendar...just kidding',
    'Can I see the calendar ?', 'I am busy for this !!!' , 'give me money first...just joking','why do you want to see that ??','check the result after a few years' , 'I am working do not bother me please','can you check it yourself ?','I am tired right now','Is it really important ???'
 ]
-let val = input.value;
 button.onclick = function getday(){
    date = new Date(input.value);
    alldays = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
    day = date.getDay();
    if(input.value = '' || alldays[day] == undefined){
       alert('please enter a valid date')
-   }else{function(){  
-    result.innerHTML = `${val} is ${alldays[day]}` 
-   }
+   }else{
+   result.innerHTML = `${input.value} is ${alldays[day]}`
    joke.innerHTML = jokes[Math.floor(Math.random() * 10)]
    input.onfocus = function(){
    location.reload()
