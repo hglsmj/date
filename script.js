f9a1ca4d1c956ccc1ef1a7d7400ea7932984b275
abood_lasheen <137902284+hglsmj@users.noreply.github.com>
2025-07-19 21:19:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ button.onclick = function getday(){
    }else{
       joke.innerHTML = jokes[Math.floor(Math.random() * 10)]
       result.innerHTML = `${date} is ${alldays[day]}`
-      input.clear();
+      input.className = 'hide';
       input.onfocus = function(){
       location.reload()
       input.value = '';
